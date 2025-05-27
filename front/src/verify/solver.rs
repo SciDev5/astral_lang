c8792dd81ecddb437a24be9fb14c054302041d04
SciDev5 <skhuber0@gmail.com>
2025-05-27 16:42:19 -0400

    implement solver unify

--- front/src/verify/solver.rs
+++ front/src/verify/solver.rs
@@ -186,6 +186,220 @@ impl Solver {
         id
     }
 
+    /// Flatten substitution symbols to minimize hops, and reduce loops to their most trivial form.
+    ///
+    /// Returns false if a self-referential concrete data-type is detected.
+    fn simplify_subs(&mut self, sym: SymbolId) -> bool {
+        fn simplify(symbols: &mut Vec<Symbol>, sym: SymbolId, trace: &mut Vec<SymbolId>) -> bool {
+            if trace.contains(&sym) && !matches!(&symbols[sym], Symbol::Subs { .. }) {
+                return false;
+            }
+            trace.push(sym);
+            let ok = match &symbols[sym] {
+                Symbol::Data { bindings, .. }
+                | Symbol::Function { bindings, .. }
+                | Symbol::MetatypeFunction { bindings, .. } => bindings
+                    .clone()
+                    .into_iter()
+                    .all(|sym| simplify(symbols, sym, trace)),
+                Symbol::Reference { inner_data } => simplify(symbols, *inner_data, trace),
+                Symbol::Metavar { .. } => todo!(),
+                Symbol::Subs { .. } => simplify_flatten_subs(symbols, sym, trace),
+            };
+            trace.pop();
+            ok
+        }
+        fn simplify_flatten_subs(
+            symbols: &mut Vec<Symbol>,
+            mut sym: SymbolId,
+            trace: &mut Vec<SymbolId>,
+        ) -> bool {
+            let mut subs_trace = Vec::from([sym]);
+            let ok = loop {
+                match &symbols[sym] {
+                    Symbol::Subs { to } => {
+                        sym = *to;
+                        subs_trace.push(sym);
+                        if subs_trace.contains(&to) {
+                            // unresolved type, break to point all above to last (including itself,
+                            // to create the trivial loop, representing the unknown)
+                            break true;
+                        }
+                    }
+                    _ => {
+                        break simplify(symbols, sym, trace);
+                    }
+                }
+            };
+            for sym in subs_trace {
+                symbols[sym] = Symbol::Subs { to: sym };
+            }
+            ok
+        }
+        let mut trace = Vec::new();
+        return simplify(&mut self.module.symbols, sym, &mut trace);
+    }
+
+    /// Makes two symbols represent the same type if possible, otherwise returns `Err`.
+    fn unify(&mut self, sym_a: SymbolId, sym_b: SymbolId) -> Result<(), AVerifyError> {
+        if !self.simplify_subs(sym_a) || !self.simplify_subs(sym_b) {
+            todo!("deal with recursive types");
+        }
+        let subs = self.unify_immut(sym_a, sym_b, HashMap::new())?;
+        for (from, to) in subs {
+            self.module.symbols[from] = Symbol::Subs { to };
+        }
+        Ok(())
+    }
+
+    fn unify_bindings(
+        &self,
+        bindings_0: &Vec<SymbolId>,
+        bindings_1: &Vec<SymbolId>,
+        existing_subs: HashMap<SymbolId, SymbolId>,
+    ) -> Result<HashMap<SymbolId, SymbolId>, AVerifyError> {
+        assert_eq!(bindings_0.len(), bindings_1.len());
+        let bindings_0 = bindings_0.iter().copied();
+        let bindings_1 = bindings_1.iter().copied();
+        (bindings_0.zip(bindings_1)).fold(Ok(existing_subs), |subs, (binding_0, binding_1)| {
+            subs.and_then(|subs| self.unify_immut(binding_0, binding_1, subs))
+        })
+    }
+    /// Makes two symbols represent the same type if possible, otherwise returns
+    /// `Err`.
+    ///
+    /// May only be called after sym_a and sym_b have been passed through [`Self::simplify_subs`].
+    fn unify_immut(
+        &self,
+        sym_a: SymbolId,
+        sym_b: SymbolId,
+        existing_subs: HashMap<SymbolId, SymbolId>,
+    ) -> Result<HashMap<SymbolId, SymbolId>, AVerifyError> {
+        /*
+        - [done] unify needs to be able to backtrack on contradiction, (returns hashmap of ids to ids for overridden symbols?)
+        - [done] want some function to simplify recursive substitution loops into the trivial representation of unbound (subs to self), would need to collect impls
+        - [done by default] need some function to move metatype impls to the types they point to
+        - [done in final verify step] unify needs to be able to ensure merge metatype impls are valid or can be validated (this can be done at the end after all unification complete)
+         */
+
+        match (&self.module.symbols[sym_a], &self.module.symbols[sym_b]) {
+            (
+                Symbol::Data {
+                    data_id: data_id_0,
+                    bindings: bindings_0,
+                },
+                Symbol::Data {
+                    data_id: data_id_1,
+                    bindings: bindings_1,
+                },
+            ) => {
+                if data_id_0 == data_id_1 {
+                    self.unify_bindings(bindings_0, bindings_1, existing_subs)
+                } else {
+                    Err(todo!("unify error"))
+                }
+            }
+            (
+                Symbol::Function {
+                    function_id: function_id_0,
+                    bindings: bindings_0,
+                },
+                Symbol::Function {
+                    function_id: function_id_1,
+                    bindings: bindings_1,
+                },
+            ) => {
+                if function_id_0 == function_id_1 {
+                    self.unify_bindings(bindings_0, bindings_1, existing_subs)
+                } else {
+                    Err(todo!("unify error"))
+                }
+            }
+            (
+                Symbol::MetatypeFunction {
+                    metatype_id: metatype_id_0,
+                    function_id: function_id_0,
+                    bindings: bindings_0,
+                },
+                Symbol::MetatypeFunction {
+                    metatype_id: metatype_id_1,
+                    function_id: function_id_1,
+                    bindings: bindings_1,
+                },
+            ) => {
+                if metatype_id_0 == metatype_id_1 && function_id_0 == function_id_1 {
+                    self.unify_bindings(bindings_0, bindings_1, existing_subs)
+                } else {
+                    Err(todo!("unify error"))
+                }
+            }
+            (
+                Symbol::Metavar {
+                    where_id: where_id_0,
+                    var_id: var_id_0,
+                },
+                Symbol::Metavar {
+                    where_id: where_id_1,
+                    var_id: var_id_1,
+                },
+            ) => {
+                if where_id_0 == where_id_1 && var_id_0 == var_id_1 {
+                    Ok(existing_subs)
+                } else {
+                    Err(todo!("unify error"))
+                }
+            }
+            (
+                Symbol::Reference {
+                    inner_data: inner_data_0,
+                },
+                Symbol::Reference {
+                    inner_data: inner_data_1,
+                },
+            ) => self.unify_immut(*inner_data_0, *inner_data_1, existing_subs),
+            (Symbol::Subs { to: to_0 }, Symbol::Subs { to: to_1 }) if *to_0 == *to_1 => {
+                Ok(existing_subs)
+            }
+            (Symbol::Subs { to }, _) => match *existing_subs.get(to).unwrap_or(to) {
+                to if sym_a == to => Ok({
+                    let mut subs = existing_subs;
+                    subs.insert(sym_a, sym_b); // [a:unk]->[b:concrete]
+                    subs
+                }),
+                sym_a => self.unify_immut(sym_a, sym_b, existing_subs),
+            },
+            (_, Symbol::Subs { to }) => match *existing_subs.get(to).unwrap_or(to) {
+                to if sym_b == to => Ok({
+                    let mut subs = existing_subs;
+                    subs.insert(sym_b, sym_a); // [b:unk]->[a:concrete]
+                    subs
+                }),
+                sym_b => self.unify_immut(sym_a, sym_b, existing_subs),
+            },
+            _ => Err(todo!("unify error")),
+        }
+    }
+    /*
+    is it possible to end up with infinite recursive structure {A->B, B->A} from simplified structure via only unify steps spawned of other unify calls.
+
+    X[A,B] & X[B,A]
+    => [A & B], [B & A]
+    => {A->B}, [B & A]
+    => {A->B} + [B & B]
+    => {A->B} + {}
+    => {A->B}
+
+
+
+     */
+
+    /*
+    fn factorial(x) =
+        x == 0
+            ? panic() // no constraint
+            : factorial(x) * x   // ret = temp2 = ret :: where Mul(temp2)
+
+     */
     /*
 
     // ret = nullable[ret]
